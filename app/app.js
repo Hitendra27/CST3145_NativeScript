8265405ed19b2cfe248228766796a65402812e71
Hitendra Quessou <hq046@live.mdx.ac.uk>
2021-03-08 23:04:01 +0000

    first commit

---
+++ app/app.js
@@ -0,0 +1,18 @@
+import Vue from 'nativescript-vue';
+
+import PetShop from './components/App';
+
+// Uncommment the following to see NativeScript-Vue output logs
+// Vue.config.silent = false;
+
+new Vue({
+
+    template: `
+        <Frame>
+            <PetShop />
+        </Frame>`,
+
+    components: {
+        PetShop
+    }
+}).$start();
---
+++ app/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <Page style="background-color: azure;">
+        <ActionBar title="Pet Shop"/>
+       <Label text="Welcome to the Pet Shop!" class="h2"/>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        };
+    },
+}
+</script>
+
+<style scoped>
+.home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+}
+
+.description-label {
+    margin-bottom: 15;
+}
+</style>
